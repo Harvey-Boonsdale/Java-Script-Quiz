56f882920e601621a3445e4e874e350cac2a0a6c
Harvey-Boonsdale <grahampeterbooth@gmail.com>
2022-10-08 17:02:34 +0100

    Having issues with script

---
+++ quiz.js
@@ -0,0 +1,73 @@
+//List of questions and answers as arrays
+
+let question1 = {
+    number: "one",
+    question: "A tandoor is a type of...",
+    option1: "Bread",
+    option2: "Oven",
+    option3: "Rice",
+    option4: "Cooking Dish",
+    answer: "Oven"
+}
+
+let question2 = {
+    number: "two",
+    question: "Gobi Aloo is...",
+    option1: "Lamb chops",
+    option2: "Spinach and potato",
+    option3: "Cauliflower and potato",
+    option4: "Aubergine and tomato",
+    answer: "Cauliflower and potato"
+}
+
+let question3 = {
+    number: "three",
+    question: "The Balti Triangle is in...",
+    option1: "Birmingham",
+    option2: "Manchester",
+    option3: "London",
+    option4: "Bradford",
+    answer: "Birmingham"
+}
+
+let question4 = {
+    number: "four",
+    question: "Vindaloo is a dish associated with...",
+    option1: "Gujarat",
+    option2: "Goa",
+    option3: "Tamil Nadu",
+    option4: "Delhi",
+    answer: "Goa"
+}
+
+let question5 = {
+    number: "five",
+    question: "Paneer is a type of...",
+    option1: "Meat",
+    option2: "Vegetable",
+    option3: "Bread",
+    option4: "Cheese",
+    answer: "Cheese"
+}
+
+let myQuestions = [question1, question2, question3, question4, question5]
+
+// Loop to generate questions and options
+
+myQuestions.forEach((i) => {
+    console.log(i):
+});
+
+function displayQuestionNumber(){
+    document.getElementById("questionNumber").innerHTML = number}
+
+displayQuestionNumber();
+
+
+// Function to check answer and keep score
+
+function submitAnswer(){
+    document.getElementById ("")
+}
+
+// Display score
